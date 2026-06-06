78ae416757a576dc98297cc9e523d90d06178e8c
Patrik Jakab <patrikjak@gmail.com>
2026-06-06 19:53:39 +0200

    remove miss click

--- resources/views/common/email-footer.blade.php
+++ resources/views/common/email-footer.blade.php
@@ -1,4 +1,4 @@
-c<div {{ $attributes->merge(['class' => 'pajak-email-footer']) }} style="background: #F7F9FF; border-top: 1px solid #EEF2FB; border-radius: 0 0 14px 14px; padding: 24px 40px; text-align: center;">
+<div {{ $attributes->merge(['class' => 'pajak-email-footer']) }} style="background: #F7F9FF; border-top: 1px solid #EEF2FB; border-radius: 0 0 14px 14px; padding: 24px 40px; text-align: center;">
     @isset($logo)
         <div class="pajak-email-footer__logo" style="display: flex; justify-content: center; margin-bottom: 10px;">{{ $logo }}</div>
     @endisset
